687a6368257b9d02b053b0159903748da136a878
Kaloyan Kosev <superkalo@devlabs.bg>
2024-07-27 01:53:03 +0300

    Change: migrate Gulp to v5

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,27 @@
-var elixir = require('laravel-elixir');
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const cssmin = require("gulp-cssmin");
+const concat = require("gulp-concat");
 
-config.assetsPath = '.';
-config.publicPath = '.';
+function compileSass() {
+  return gulp
+    .src("sass/style.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("css"));
+}
 
-elixir(function(mix) {
-    mix
-    .sass('/style.scss', 'css/style.css')
-    .styles([
-            '../lib/normalize-css/normalize.css',
-            '../lib/TooltipStylesInspiration/css/tooltip-flip.css',
-            'css/style.css'
-        ], 'css/style.min.css');
-});
+function minifyCss() {
+  return gulp
+    .src([
+      "lib/normalize-css/normalize.css",
+      "lib/TooltipStylesInspiration/css/tooltip-flip.css",
+      "css/style.css",
+    ])
+    .pipe(concat("style.min.css"))
+    .pipe(cssmin())
+    .pipe(gulp.dest("css"));
+}
+
+const build = gulp.series(compileSass, minifyCss);
+
+exports.default = build;
